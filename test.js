02a34cd6431450b7c2388627ae1559ab9823279e
wolf-yuan-6115 <h1030035@gmail.com>
2021-06-27 13:38:44 +0800

    Remove branch limit due testing

---
+++ test.js
@@ -0,0 +1,14 @@
+const { readdirSync } = require("fs");
+
+console.log("Testing commands...");
+
+const commandFiles = readdirSync(join(__dirname, "commands")).filter((file) => file.endsWith(".js"));
+for (const file of commandFiles) {
+  try {
+    require(join(__dirname, "commands", `${file}`));
+  } catch(e) {
+    console.log(`Found error when testing command ${file}`, e);
+    process.exit(1);
+  }
+  console.log(`Tested command ${file}`);
+}
